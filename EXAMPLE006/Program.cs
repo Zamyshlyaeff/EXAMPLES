3c89340df59bdecfd9608658e41857f5c671729e
Nikolayz <zans55@yandex.ru>
2023-02-03 11:49:58 +0300

    EXAMPLE006

---
+++ EXAMPLE006/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 6;
+int c = 2;
+int d = 5;
+int e = 3;
+
+int max=a;
+
+if (a > max ) max = a; 
+if (b > max ) max = b;
+if (c > max ) max = c;  
+if (d > max ) max = d; 
+if (e > max ) max = e; 
+ Console.Write("Максимальное число равно ");
+Console.Write(max);
+
