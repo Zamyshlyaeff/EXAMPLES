0e0b79607f47a172c54f39846e512091c0f5f2cb
Nikolayz <zans55@yandex.ru>
2023-02-05 08:54:20 +0300

    EXMPL10

---
+++ EXMPL10/Program.cs
@@ -0,0 +1,16 @@
+﻿//  
+int [] array = {1,51,22,98,33,54,22};
+int n = array.Length;
+int find =22;
+int index =0;
+
+while (index < n)
+{
+if (array[index]==find)
+{
+    Console.WriteLine(index);
+    break;
+}     //index+1;
+      index++;
+}
+
